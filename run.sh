e200248c3ceb0e74b46b31803fdb2166188fe3f0
Berend-Jan Lange <berendjanlange@Berend-Jans-MacBook-Pro.local>
2024-02-24 17:26:56 +0100

    added run.sh

---
+++ run.sh
@@ -0,0 +1,13 @@
+# compile
+zokrates compile -i root.zok
+# perform the setup phase
+zokrates setup
+# execute the program
+zokrates compute-witness -a 337 113569
+# generate a proof of computation
+zokrates generate-proof
+# export a solidity verifier
+zokrates export-verifier
+# or verify natively
+zokrates verify
+
